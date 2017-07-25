3bf3eb299c4deeff5e1eef0a067cf02b7bb89dce
Ahmed Jalal <ahmed.dev.jalal@gmail.com>
2017-07-25 19:42:47 +0200

    logo issue two.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
         <div class="navbar-header flex-item">
           <div class="navbar-header">
               <div class="navbar-brand">
-                    <img src="{{ URL::to('/public/icons/icon.png') }}" alt="The BoOk club">
+                    <img src="{{ URL::to('../public/icons/icon.png') }}" alt="The BoOk club">
                     The BoOk club
                 </div>
 
