65bdc0ae99ea9dd501dc9a6299492f11e2b5c29c
Ahmed Jalal <ahmed.dev.jalal@gmail.com>
2017-07-25 20:02:01 +0200

    logo issue final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
           <div class="navbar-header">
               <div class="navbar-brand">
 
-                    <img src="{{ asset('/icons/icon.png') }}" alt="The BoOk club">
+                    <img src="{{ asset('/public/icons/icon.png') }}" alt="The BoOk club">
                     The BoOk club
                 </div>
 
